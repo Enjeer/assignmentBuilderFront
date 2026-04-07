2672c7f5a73c3add2d4ad8b77029293528513185
NAI - VIIjeer <wallesalex205@gmail.com>
2026-04-08 00:34:46 +0300

    fix: speed insights import changed from next to react

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import EditorPage from "@/pages/EditorPage";
 import SupportPage from "./pages/SupportPage";
 import NotFound from "@/pages/NotFound";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
